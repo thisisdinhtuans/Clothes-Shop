65454414e1468a6d503975fa9e718bc745228bb9
thisisdinhtuans <dinhmanhtuans89@gmail.com>
2024-03-05 16:58:35 +0700

    component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,19 @@
+const products=[
+  {name:'product1', price:100.00},
+  {name:'product2', price:200.00},
+  {name:'product2', price:200.00},
+
+]
+
 function App() {
   return (
     <div>
       <h1>Re-Store</h1>
+      <ul>
+        {products.map((item, index)=>(
+          <li key={index}>{item.name} - {item.price}</li>
+        ))}
+      </ul>
     </div>
   )
 }
