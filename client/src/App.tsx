a9ed5b6fe1f347a669105e4f1a3174e4cfde1449
thisisdinhtuans <dinhmanhtuans89@gmail.com>
2024-03-07 12:29:36 +0700

    Typescript interface for product

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,9 @@
 import { useEffect, useState } from "react";
+import { Product } from "./product";
 
 function App() {
   //sử dụng hook useState để khởi tạo một state products là 1 mảng chứa danh sách các sản phẩm. Mỗi sản phẩm được đại diện bởi một đối tượng có 2 thuocj tish là name và price
-  const [products, setProducts]=useState([
-    {name:'product1', price:100.00},
-    {name:'product2', price:200.00},
-  ]);
+  const [products, setProducts]=useState<Product[]>([]);
   //sử dụng hook useEffect trong React để thực hiện các thao tác có liên quan đến slide effects, chẳng hạn như gọi api khi component được render
   //1. useEffect nhận vào 1 hàm callBack và 1 mảng dependency. Trong trường hợp mảng dependency được truyền vào là [], nghĩa là useEffect sẽ chỉ được gọi 
   // một lần sau khi component được render lần đầu tiên và không có dependency nào được cung cấp.
@@ -21,7 +19,15 @@ function App() {
 
   //hàm addProduct để định nghĩa để thêm 1 sản phẩm mới vào danh sách products, hàm nầy sử dụng setProducts để cập nhật state products. Thay vì trực tiếp sử dụng mảng products hiện tại, nó sử dụng sử dụng hàm callback của setProducts và truyền vào tham số prevState, đại diện cho giá trị trước đó của products. Sau đó, ns tạo ra 1 mảng mới bằng cash sao chép prevState và thêm 1 sản phẩm mới vào cuối mảng đó
   function addProduct() {
-    setProducts(prevState=>[...prevState, {name:'product'+(prevState.length+1), price: (prevState.length*100)+100}]);
+    setProducts(prevState=>[...prevState, {
+      id:prevState.length+1,
+      name:'product'+(prevState.length+1), 
+      price: (prevState.length*100)+100,
+      brand: 'some brand',
+      description: 'some description',
+      pictureUrl: 'http://picsum.photos/200'
+    
+    }]);
   }
   //đây là phần trả về component
   return (
@@ -29,8 +35,8 @@ function App() {
       <h1>Re-Store</h1>
       <ul>
         {/* sử dụng phương thức map để lặp qua mảng products và render mỗi sản phẩm với name và price*/}
-        {products.map((item, index)=>(
-          <li key={index}>{item.name} - {item.price}</li>
+        {products.map(product=>(
+          <li key={product.id}>{product.name} - {product.price}</li>
         ))}
       </ul>
       {/* nút addProduct được hiển thị bên dưới danh sách sản phẩm , khi người dùng bấm vào nút này thì hàm addProduct() sẽ được gọi để thêm 1 sản phẩm mới vào danh sách */}
