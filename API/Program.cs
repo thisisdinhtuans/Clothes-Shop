069ec016429ea4fe589b2ab5694130d7034c8c17
thisisdinhtuans <dinhmanhtuans89@gmail.com>
2024-03-07 11:06:25 +0700

    adding CORS configuration to the API

--- API/Program.cs
+++ API/Program.cs
@@ -15,6 +15,8 @@ builder.Services.AddDbContext<StoreContext>(opt=>{
     //GetConnectionString lấy từ Configuration nha
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+//Dòng này được sử dụng để thêm dịch vụ CORS vào hệ thống dịch vụ của ứng dụng.
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,6 +26,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+//Đây là cấu hình cụ thể của CORS. Bạn đang chỉ định rằng bất kỳ trang web nào có nguồn gốc từ http://localhost:3000 (domain này) được phép gửi yêu cầu tới API của bạn. 
+//AllowAnyHeader() cho phép bất kỳ header nào trong yêu cầu, 
+//AllowAnyMethod() cho phép bất kỳ phương thức nào (GET, POST, PUT, DELETE, vv.), và .WithOrigins("http://localhost:3000") chỉ định nguồn gốc cụ thể được phép gửi yêu cầu.
+app.UseCors(opt=>{
+    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+});
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,11 +6,17 @@ function App() {
     {name:'product1', price:100.00},
     {name:'product2', price:200.00},
   ]);
-
+  //sử dụng hook useEffect trong React để thực hiện các thao tác có liên quan đến slide effects, chẳng hạn như gọi api khi component được render
+  //1. useEffect nhận vào 1 hàm callBack và 1 mảng dependency. Trong trường hợp mảng dependency được truyền vào là [], nghĩa là useEffect sẽ chỉ được gọi 
+  // một lần sau khi component được render lần đầu tiên và không có dependency nào được cung cấp.
+  //2. trong hàm callBack của useEffect, một yêu cầu fetch được thực hiện đến endpoint 'http://localhost:5000/api/Products' để lấy dữ liệu về sản phẩm. Đây là 1 api sử dụng phương thức GET
+  //3. sau khi nhận được phản hồi từ api dưới dạng json, hàm .json() được gọi để chuyển dữ liệu phản hòi thành dạng JSON
+  //4. dữ liệu JSON đó được chuyển đến hàm setProducts để cập nhật state 'products' trong component React với dữ liệu mới lấy từ api. Khi setProducts được gọi, component sẽ được render lại với dữ liệu sản phẩm mới
+  //Tóm lại, đoạn mã trên sử dụng useEffect để gọi API khi component được render lần đầu tiên và cập nhật state products của component với dữ liệu sản phẩm từ API.
   useEffect(()=> {
-    fetch('http://localhost:5000/api/products')
-    .then(response=>response.json())
-    .then(data=>setProducts(data))
+    fetch('http://localhost:5000/api/Products')
+      .then(response=>response.json())
+      .then(data=>setProducts(data))
   }, [])
 
   //hàm addProduct để định nghĩa để thêm 1 sản phẩm mới vào danh sách products, hàm nầy sử dụng setProducts để cập nhật state products. Thay vì trực tiếp sử dụng mảng products hiện tại, nó sử dụng sử dụng hàm callback của setProducts và truyền vào tham số prevState, đại diện cho giá trị trước đó của products. Sau đó, ns tạo ra 1 mảng mới bằng cash sao chép prevState và thêm 1 sản phẩm mới vào cuối mảng đó
