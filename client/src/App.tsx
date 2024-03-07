e87d9eea3f6158c8159545595d34dc0210f0d6e3
thisisdinhtuans <dinhmanhtuans89@gmail.com>
2024-03-07 09:07:17 +0700

    useState1

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,28 @@
-const products=[
-  {name:'product1', price:100.00},
-  {name:'product2', price:200.00},
-  {name:'product2', price:200.00},
-
-]
+import { useState } from "react";
 
 function App() {
+  //sử dụng hook useState để khởi tạo một state products là 1 mảng chứa danh sách các sản phẩm. Mỗi sản phẩm được đại diện bởi một đối tượng có 2 thuocj tish là name và price
+  const [products, setProducts]=useState([
+    {name:'product1', price:100.00},
+    {name:'product2', price:200.00},
+  ]);
+
+  //hàm addProduct để định nghĩa để thêm 1 sản phẩm mới vào danh sách products, hàm nầy sử dụng setProducts để cập nhật state products bằng cách sao chép mảng products hiện tại và thêm 1 phẩn tử mới vào cuối mảng đó
+  function addProduct() {
+    setProducts([...products, {name:'product3', price:300.00}]);
+  }
+  //đây là phần trả về component
   return (
     <div>
       <h1>Re-Store</h1>
       <ul>
+        {/* sử dụng phương thức map để lặp qua mảng products và render mỗi sản phẩm với name và price*/}
         {products.map((item, index)=>(
           <li key={index}>{item.name} - {item.price}</li>
         ))}
       </ul>
+      {/* nút addProduct được hiển thị bên dưới danh sách sản phẩm , khi người dùng bấm vào nút này thì hàm addProduct() sẽ được gọi để thêm 1 sản phẩm mới vào danh sách */}
+      <button onClick={addProduct}>Add product</button>
     </div>
   )
 }
