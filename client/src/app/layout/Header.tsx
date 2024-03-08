5bb0148e74f96eb84db39abac25eba8cf3a3664e
thisisdinhtuans <dinhmanhtuans89@gmail.com>
2024-03-08 14:56:43 +0700

    Stying the navbar

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,5 +1,5 @@
 import { ShoppingCart } from "@mui/icons-material";
-import { AppBar, Badge, IconButton, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
+import { AppBar, Badge, Box, IconButton, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
 import { NavLink } from "react-router-dom";
 
 const midLinks= [
@@ -13,6 +13,19 @@ const rightLinks= [
     {title:'register', path:'/Register'},
 ]
 
+const navStyles= {
+    color:'inherit', 
+    textDecoration:'none',
+    typography:'h6',
+    //để khi mà lướt qua thì nó sẽ có màu
+    '&:hover':{
+        color:'grey.500'
+    },
+    '&.active': {
+        color:'text.secondary'
+    }
+}
+
 interface Props {
     darkMode:boolean;
     handleThemeChange:() => void;
@@ -21,45 +34,49 @@ interface Props {
 export default function Header({darkMode, handleThemeChange}:Props) {
     return (
         <AppBar position='static' sx={{mb:4}}>
-            <Toolbar>
-                <Typography variant='h6' component={NavLink} 
-                    to='/'
-                    sx={{color:'inherit', textDecoration:'none'}}    
-                >
-                    RE-STORE
-                </Typography>
-                <Switch checked={darkMode} onChange={handleThemeChange}/>
+            <Toolbar sx={{display:'flex', justifyContent:'space-between', alignItems:'center'}}>
+                <Box display='flex' alignItems='center'>
+                    <Typography variant='h6' component={NavLink} 
+                        to='/'
+                        sx={navStyles}    
+                    >
+                        RE-STORE
+                    </Typography>
+                    <Switch checked={darkMode} onChange={handleThemeChange}/>
+                </Box>
                 <List sx={{display:'flex'}}>
                     {midLinks.map(({title, path})=>(
                         <ListItem
                             component={NavLink}
                             to={path}
                             key={path}
-                            sx={{color:'inherit', typography:'h6'}}
+                            sx={navStyles}
                         >
                             {title.toUpperCase()}
                         </ListItem>
                     ))}
                 </List>
+                {/* //display='flex' alignItems='center' để cho nó ngang hàng vs nhau */}
+                <Box display='flex' alignItems='center'>
+                    <IconButton size='large' edge='start' color='inherit' sx={{mr:2}}>
+                        <Badge badgeContent='4' color="secondary">
+                            <ShoppingCart />
+                        </Badge>
+                    </IconButton>
 
-                <IconButton size='large' edge='start' color='inherit' sx={{mr:2}}>
-                    <Badge badgeContent='4' color="secondary">
-                        <ShoppingCart />
-                    </Badge>
-                </IconButton>
-
-                <List sx={{display:'flex'}}>
-                    {rightLinks.map(({title, path})=>(
-                        <ListItem
-                            component={NavLink}
-                            to={path}
-                            key={path}
-                            sx={{color:'inherit', typography:'h6'}}
-                        >
-                            {title.toUpperCase()}
-                        </ListItem>
-                    ))}
-                </List>
+                    <List sx={{display:'flex'}}>
+                        {rightLinks.map(({title, path})=>(
+                            <ListItem
+                                component={NavLink}
+                                to={path}
+                                key={path}
+                                sx={navStyles}
+                            >
+                                {title.toUpperCase()}
+                            </ListItem>
+                        ))}
+                    </List>
+                </Box>
             </Toolbar>
         </AppBar>
     )
