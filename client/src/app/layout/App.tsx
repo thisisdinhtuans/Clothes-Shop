4d2549698a4ddaab01c26098b61955b1dbc8bf22
thisisdinhtuans <dinhmanhtuans89@gmail.com>
2024-03-08 10:32:30 +0700

    dark mode

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,47 +1,35 @@
-import { useEffect, useState } from "react";
-import { Product } from "../models/product";
 import Catalog from "../../features/catalog/Catalog";
-import { Container, CssBaseline, Typography } from "@mui/material";
+import { Container, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+// import {Theme} from "@material-ui/core";
+// import {Theme} from "@material-ui/core";
 import Header from "./Header";
+import { useState } from "react";
 
 function App() {
-  //sử dụng hook useState để khởi tạo một state products là 1 mảng chứa danh sách các sản phẩm. Mỗi sản phẩm được đại diện bởi một đối tượng có 2 thuocj tish là name và price
-  const [products, setProducts]=useState<Product[]>([]);
-  //sử dụng hook useEffect trong React để thực hiện các thao tác có liên quan đến slide effects, chẳng hạn như gọi api khi component được render
-  //1. useEffect nhận vào 1 hàm callBack và 1 mảng dependency. Trong trường hợp mảng dependency được truyền vào là [], nghĩa là useEffect sẽ chỉ được gọi 
-  // một lần sau khi component được render lần đầu tiên và không có dependency nào được cung cấp.
-  //2. trong hàm callBack của useEffect, một yêu cầu fetch được thực hiện đến endpoint 'http://localhost:5000/api/Products' để lấy dữ liệu về sản phẩm. Đây là 1 api sử dụng phương thức GET
-  //3. sau khi nhận được phản hồi từ api dưới dạng json, hàm .json() được gọi để chuyển dữ liệu phản hòi thành dạng JSON
-  //4. dữ liệu JSON đó được chuyển đến hàm setProducts để cập nhật state 'products' trong component React với dữ liệu mới lấy từ api. Khi setProducts được gọi, component sẽ được render lại với dữ liệu sản phẩm mới
-  //Tóm lại, đoạn mã trên sử dụng useEffect để gọi API khi component được render lần đầu tiên và cập nhật state products của component với dữ liệu sản phẩm từ API.
-  useEffect(()=> {
-    fetch('http://localhost:5000/api/Products')
-      .then(response=>response.json())
-      .then(data=>setProducts(data))
-  }, [])
+  const [darkMode, setDarkMode]=useState(false);
+  const paletteType=darkMode ? 'dark':'light';
+  const theme=createTheme({
+    palette: {
+      mode: paletteType,
+      background: {
+        default:paletteType==='light' ? '#eaeaea' :'#121212'
+      }
+    }
+  })
 
-  //hàm addProduct để định nghĩa để thêm 1 sản phẩm mới vào danh sách products, hàm nầy sử dụng setProducts để cập nhật state products. Thay vì trực tiếp sử dụng mảng products hiện tại, nó sử dụng sử dụng hàm callback của setProducts và truyền vào tham số prevState, đại diện cho giá trị trước đó của products. Sau đó, ns tạo ra 1 mảng mới bằng cash sao chép prevState và thêm 1 sản phẩm mới vào cuối mảng đó
-  function addProduct() {
-    setProducts(prevState=>[...prevState, {
-      id:prevState.length+1,
-      name:'product'+(prevState.length+1), 
-      price: (prevState.length*100)+100,
-      brand: 'some brand',
-      description: 'some description',
-      pictureUrl: 'http://picsum.photos/200'
-    
-    }]);
+  function handleThemeChange() {
+    setDarkMode(!darkMode);
   }
   //đây là phần trả về component
   return (
-    <>
+    <ThemeProvider theme={theme}>
     {/* làm cho Header nó phủ dọc hết */}
       <CssBaseline/>
-      <Header />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
       <Container>
-        <Catalog products={products} addProduct={addProduct}/>
+        <Catalog />
       </Container>
-    </>
+    </ThemeProvider>
   )
 }
 
--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,20 +1,28 @@
-import { Button } from "@mui/material";
 import { Product } from "../../app/models/product";
 import ProductList from "./ProductList";
-
-//Phân biệt mảng ở Props này nha, vì nó sẽ 
-interface Props {
-    products:Product[];
-    addProduct:() => void;
-}
+import { useState, useEffect } from "react";
 
 //chỗ này có thể thêm là any, tức là có thể nhận mọi thứ vào vẫn chạy bình thường, nhưng chúng ta đã khai báo interface Props ở trên thì kế thừa luôn từ cái Props đấy
-export default function Catalog({products, addProduct}:Props) {
+export default function Catalog() {
+    //sử dụng hook useState để khởi tạo một state products là 1 mảng chứa danh sách các sản phẩm. Mỗi sản phẩm được đại diện bởi một đối tượng có 2 thuocj tish là name và price
+  const [products, setProducts]=useState<Product[]>([]);
+  //sử dụng hook useEffect trong React để thực hiện các thao tác có liên quan đến slide effects, chẳng hạn như gọi api khi component được render
+  //1. useEffect nhận vào 1 hàm callBack và 1 mảng dependency. Trong trường hợp mảng dependency được truyền vào là [], nghĩa là useEffect sẽ chỉ được gọi 
+  // một lần sau khi component được render lần đầu tiên và không có dependency nào được cung cấp.
+  //2. trong hàm callBack của useEffect, một yêu cầu fetch được thực hiện đến endpoint 'http://localhost:5000/api/Products' để lấy dữ liệu về sản phẩm. Đây là 1 api sử dụng phương thức GET
+  //3. sau khi nhận được phản hồi từ api dưới dạng json, hàm .json() được gọi để chuyển dữ liệu phản hòi thành dạng JSON
+  //4. dữ liệu JSON đó được chuyển đến hàm setProducts để cập nhật state 'products' trong component React với dữ liệu mới lấy từ api. Khi setProducts được gọi, component sẽ được render lại với dữ liệu sản phẩm mới
+  //Tóm lại, đoạn mã trên sử dụng useEffect để gọi API khi component được render lần đầu tiên và cập nhật state products của component với dữ liệu sản phẩm từ API.
+  useEffect(()=> {
+    fetch('http://localhost:5000/api/Products')
+      .then(response=>response.json())
+      .then(data=>setProducts(data))
+  }, [])
+
     return (
         <>
             <ProductList products={products}/>
             {/* nút addProduct được hiển thị bên dưới danh sách sản phẩm , khi người dùng bấm vào nút này thì hàm addProduct() sẽ được gọi để thêm 1 sản phẩm mới vào danh sách */}
-            <Button variant='contained' onClick={addProduct}>Add product</Button>
         </>
     )
 }
