78bdca93c9b8c4f2f92786fde512c86d688dcb26
thisisdinhtuans <dinhmanhtuans89@gmail.com>
2024-03-13 11:53:43 +0700

    setup linting

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -39,8 +39,8 @@ axios.interceptors.response.use(async response => {
 })
 const requests={
     get:(url:string) => axios.get(url).then(responseBody),
-    post:(url:string, body:{}) => axios.post(url, body).then(responseBody),
-    put:(url:string, body:{}) => axios.put(url, body).then(responseBody),
+    post:(url:string, body:object) => axios.post(url, body).then(responseBody),
+    put:(url:string, body:object) => axios.put(url, body).then(responseBody),
     del:(url:string) => axios.delete(url).then(responseBody)
 }
 
