823cb8ae139df2b34fe863c4eac4259df9a8b7ef
thisisdinhtuans <dinhmanhtuans89@gmail.com>
2024-03-14 16:09:28 +0700

    function getCookie(key) {

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -3,11 +3,26 @@ import { Container, CssBaseline, ThemeProvider, createTheme } from "@mui/materia
 // import {Theme} from "@material-ui/core";
 // import {Theme} from "@material-ui/core";
 import Header from "./Header";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import 'react-toastify/dist/ReactToastify.css';
+import { useStoreContext } from '../context/StoreContext';
+import { getCookie } from '../util/util';
+import agent from '../api/agent';
+import LoadingComponent from './LoadingComponent';
 
 function App() {
+  const {setBasket} =useStoreContext();
+  const [loading, setLoading]=useState(true);
+  useEffect(()=>{
+    const buyerId=getCookie('buyerId');
+    if(buyerId) {
+      agent.Basket.get()
+        .then(basket=>setBasket(basket))
+        .catch(error=>console.log(error))
+        .finally(()=>setLoading(false))
+    }
+  },[setBasket])
   const [darkMode, setDarkMode]=useState(false);
   const paletteType=darkMode ? 'dark':'light';
   const theme=createTheme({
@@ -22,6 +37,8 @@ function App() {
   function handleThemeChange() {
     setDarkMode(!darkMode);
   }
+
+  if(loading) return <LoadingComponent message='Initialising app ...' />
   //đây là phần trả về component
   return (
     <ThemeProvider theme={theme}>
