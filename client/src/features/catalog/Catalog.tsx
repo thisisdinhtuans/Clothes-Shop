6b745242360da0a0b7cb2454373c1390604f9ff2
thisisdinhtuans <dinhmanhtuans89@gmail.com>
2024-03-18 13:43:41 +0700

    use entity adapter selector

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,14 +1,15 @@
-import agent from "../../app/api/agent";
 import LoadingComponent from "../../app/layout/LoadingComponent";
-import { Product } from "../../app/models/product";
+import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
 import ProductList from "./ProductList";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
+import { fetchProductsAsync, productSelectors } from "./catalogSlice";
 
 //chỗ này có thể thêm là any, tức là có thể nhận mọi thứ vào vẫn chạy bình thường, nhưng chúng ta đã khai báo interface Props ở trên thì kế thừa luôn từ cái Props đấy
 export default function Catalog() {
     //sử dụng hook useState để khởi tạo một state products là 1 mảng chứa danh sách các sản phẩm. Mỗi sản phẩm được đại diện bởi một đối tượng có 2 thuocj tish là name và price
-  const [products, setProducts]=useState<Product[]>([]);
-  const [loading, setLoading]=useState(true);
+  const products=useAppSelector(productSelectors.selectAll);
+  const {productsLoaded,status}=useAppSelector(state=>state.catalog);
+  const dispatch=useAppDispatch();
   //sử dụng hook useEffect trong React để thực hiện các thao tác có liên quan đến slide effects, chẳng hạn như gọi api khi component được render
   //1. useEffect nhận vào 1 hàm callBack và 1 mảng dependency. Trong trường hợp mảng dependency được truyền vào là [], nghĩa là useEffect sẽ chỉ được gọi 
   // một lần sau khi component được render lần đầu tiên và không có dependency nào được cung cấp.
@@ -17,12 +18,9 @@ export default function Catalog() {
   //4. dữ liệu JSON đó được chuyển đến hàm setProducts để cập nhật state 'products' trong component React với dữ liệu mới lấy từ api. Khi setProducts được gọi, component sẽ được render lại với dữ liệu sản phẩm mới
   //Tóm lại, đoạn mã trên sử dụng useEffect để gọi API khi component được render lần đầu tiên và cập nhật state products của component với dữ liệu sản phẩm từ API.
   useEffect(()=> {
-    agent.Catalog.list()
-    .then(products =>setProducts(products))
-    .catch(error=>console.log(error))
-    .finally(()=>setLoading(false))
-  }, [])
-  if(loading)return <LoadingComponent message='Loading products...'/>
+    if(!productsLoaded) dispatch(fetchProductsAsync());
+  }, [productsLoaded])
+  if(status.includes('pending'))return <LoadingComponent message='Loading products...'/>
 
     return (
         <>
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -10,9 +10,7 @@ import { router } from './app/router/Routes.tsx';
 import { StoreProvider } from './app/context/StoreContext.tsx';
 import { Provider } from 'react-redux';
 import { store } from './app/store/configureStore.ts';
-import { fetchProductsAsync } from './features/catalog/catalogSlice.ts';
 
-store.dispatch(fetchProductsAsync());
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <StoreProvider>
