20269ebe64a5fa5abd16f844ddffb5de19bc6450
thisisdinhtuans <dinhmanhtuans89@gmail.com>
2024-03-19 09:48:06 +0700

    add sorting API

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 using API.Data;
 using API.Entities;
+using API.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,8 +16,11 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts() {
-            return await _context.Products.ToListAsync();
+        public async Task<ActionResult<List<Product>>> GetProducts(string orderBy) {
+            var query= _context.Products
+                .Sort(orderBy)
+                .AsQueryable();
+            return await query.ToListAsync();
         }
 
         [HttpGet("{id}")] //api/products/3
