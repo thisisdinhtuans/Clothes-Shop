be42083ec261acf888343aba6c849a2d5dfdc23a
thisisdinhtuans <dinhmanhtuans89@gmail.com>
2024-03-27 09:29:31 +0700

    Validate token & Configure swagger to send the auth token

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,13 @@
+using System.Text;
 using API.Data;
 using API.Entities;
 using API.Middleware;
 using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,7 +17,32 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c=>
+{
+    var jwtSecurityScheme=new OpenApiSecurityScheme
+    {
+        BearerFormat="JWT",
+        Name="Authorization",
+        In=ParameterLocation.Header,
+        Type=SecuritySchemeType.ApiKey,
+        Scheme=JwtBearerDefaults.AuthenticationScheme,
+        Description="Put Bearer + your token in the box below",
+        Reference = new OpenApiReference
+        {
+            Id=JwtBearerDefaults.AuthenticationScheme,
+        Type=ReferenceType.SecurityScheme,
+        }
+    };
+
+    c.AddSecurityDefinition(jwtSecurityScheme.Reference.Id, jwtSecurityScheme);
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            jwtSecurityScheme, Array.Empty<string>()
+        }
+    });
+});
 
 builder.Services.AddDbContext<StoreContext>(opt=>{
     //GetConnectionString lấy từ Configuration nha
@@ -28,7 +57,18 @@ builder.Services.AddIdentityCore<User>(opt=>{
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<StoreContext>();
 
-builder.Services.AddAuthentication();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(opt=>
+    {
+        opt.TokenValidationParameters=new TokenValidationParameters 
+        {
+            ValidateIssuer=false,
+            ValidateAudience=false,
+            ValidateLifetime=true,
+            ValidateIssuerSigningKey=true,
+            IssuerSigningKey=new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWTSettings:TokenKey"]))
+        };
+    });
 builder.Services.AddAuthorization();
 builder.Services.AddScoped<TokenService>();
 var app = builder.Build();
@@ -38,7 +78,10 @@ app.UseMiddleware<ExceptionMiddleware>();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c=>
+    {
+        c.ConfigObject.AdditionalItems.Add("persistAuthorization","true");
+    });
 }
 
 //Đây là cấu hình cụ thể của CORS. Bạn đang chỉ định rằng bất kỳ trang web nào có nguồn gốc từ http://localhost:3000 (domain này) được phép gửi yêu cầu tới API của bạn. 
@@ -51,7 +94,7 @@ app.UseCors(opt=>{
     opt.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:3000");
 });
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
