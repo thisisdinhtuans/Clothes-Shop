4016e94fe787cb1e53f5a8088a700d0b0e2aa755
thisisdinhtuans <dinhmanhtuans89@gmail.com>
2024-03-28 14:32:26 +0700

    add account slice

--- client/src/features/account/Login.tsx
+++ client/src/features/account/Login.tsx
@@ -5,23 +5,24 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { Box, Paper } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import agent from '../../app/api/agent';
 import { FieldValues, useForm } from 'react-hook-form';
 import { LoadingButton } from '@mui/lab';
+import { useAppDispatch } from '../../app/store/configureStore';
+import { signInUser } from './accountSlice';
 
 
 export default function Login() {
+  const navigate=useNavigate();
+  const dispatch=useAppDispatch();
     const {register, handleSubmit, formState: {isSubmitting, errors, isValid}}=useForm( {
         mode:'onTouched'
     })
 
-    function submitForm(data:FieldValues) {
-        try {
-            agent.Account.login(data);
-        } catch(error) {
-            console.log(error);
-        }
+    async function submitForm(data:FieldValues) {
+        await dispatch(signInUser(data));
+        navigate('/catalog');
     }
   return (
       <Container component={Paper} maxWidth="sm" 
