5fb88a8b2215f877a88c95be3805538ece38a5b3
thisisdinhtuans <dinhmanhtuans89@gmail.com>
2024-03-31 15:39:27 +0700

    create order entities and refactoring identity to use

--- API/Program.cs
+++ API/Program.cs
@@ -54,7 +54,7 @@ builder.Services.AddCors();
 builder.Services.AddIdentityCore<User>(opt=>{
         opt.User.RequireUniqueEmail=true;
     })
-    .AddRoles<IdentityRole>()
+    .AddRoles<Role>()
     .AddEntityFrameworkStores<StoreContext>();
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
